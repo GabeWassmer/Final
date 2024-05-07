dd1577529798ba9a6e2f289380be4f23257116ec
Gabe Wassmer <gwassmer@students.ccu.edu>
2024-05-07 08:41:21 -0600

    FinalAppCommit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿public class AgeCalculator
+{
+    public static int CalculateAgeInDays()
+    {
+        Console.WriteLine("What's your birthday?");
+        DateTime birthday;
+        while (!DateTime.TryParse(Console.ReadLine(), out birthday))
+        {
+            Console.WriteLine("Invalid date format.");
+        }
+
+        TimeSpan ageInDays = DateTime.Now - birthday;
+        return ageInDays.Days;
+    }
+
+    public static void Main(string[] args)
+    {
+        int ageInDays = CalculateAgeInDays();
+        Console.WriteLine($"You are: {ageInDays} days old.");
+        Console.ReadLine();
+    }
+}
